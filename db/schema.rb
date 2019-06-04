da54a357a9dd734728a13aabf67df8e9790a6a1a
Ana Negura <ana_n21@yahoo.com>
2019-06-04 13:00:34 +0200

    geocoder

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_03_143354) do
+ActiveRecord::Schema.define(version: 2019_06_04_104956) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 2019_06_03_143354) do
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["city_id"], name: "index_activities_on_city_id"
   end
 
@@ -35,6 +37,8 @@ ActiveRecord::Schema.define(version: 2019_06_03_143354) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "step_activities", force: :cascade do |t|
