d197b93565c4e34a4e839da072cf2fe33c2cd03c
Nicolas Minguet <nicolas.minguet@hotmail.fr>
2019-06-04 18:34:43 +0200

    trip 101

--- db/migrate/20190603142224_create_trips.rb
+++ db/migrate/20190603142224_create_trips.rb
@@ -2,8 +2,8 @@ class CreateTrips < ActiveRecord::Migration[5.2]
   def change
     create_table :trips do |t|
       t.references :user, foreign_key: true
-      t.string :departure_city
-      t.string :arrival_city
+      t.references :arrival_city, foreign_key: {to_table: :cities}
+      t.references :departure_city, foreign_key: {to_table: :cities}
       t.datetime :start_date
       t.datetime :end_date
       t.json :criteria
