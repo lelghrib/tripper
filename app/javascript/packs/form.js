2321ccfaa5dc8af9ef8dce89b261d150bef132ff
Ana Negura <ana_n21@yahoo.com>
2019-06-06 18:52:20 +0200

    only two chewcked

--- app/javascript/packs/form.js
+++ app/javascript/packs/form.js
@@ -1,4 +1,4 @@
-const checkboxclick = () => {
+const checkboxclic = () => {
 document.querySelectorAll(".activity-choice").forEach((element) =>{
   element.addEventListener("click", (event) => {
     event.currentTarget.classList.toggle("active");
@@ -6,5 +6,29 @@ document.querySelectorAll(".activity-choice").forEach((element) =>{
 });
 }
 
-export { checkboxclick };
+const checkboxclick = () => {
+  $(document).ready(function(){
+    $(".activity-choice").click(function(){
+      if ($(this).hasClass('active')) {
+        $(this).removeClass("active");
+      } else {
+        if($('.activity-choice.active').length < 2) {
+         $(this).addClass("active");
+        }
+      }
+
+    });
+  });
+}
+
+const checktwo = () => {
+$('.check-culture').on('change', function() {
+   if($('.check-culture:checked').length > 2) {
+       this.checked = false;
+   }
+});
+}
+
+
+export { checkboxclick, checktwo };
 
