4925c854ded6b354f6ced910abf92c080a5f9f7b
jeanremi bertel <jeanremi.bertel@gmail.com>
2019-06-10 12:18:37 +0200

    modifi hp and so on

--- app/javascript/plugins/slider.js
+++ app/javascript/plugins/slider.js
@@ -1,6 +1,6 @@
 window.onload=function(){
   $('.slider').slick({
-  autoplay:false,
+  autoplay:true,
   autoplaySpeed:1500,
   arrows:true,
   prevArrow:'<button type="button" class="slick-prev"></button>',
