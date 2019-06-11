24841090907db4224db14ca224f5797470f229dd
Ana Negura <ana_n21@yahoo.com>
2019-06-11 16:10:18 +0200

    info window and km' '

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -57,6 +57,8 @@ class TripsController < ApplicationController
       leg = response["trips"][0]["legs"][i]
       this_step = @trip.steps.find{ |st| step['location'][0].round(2) == st.city.longitude.round(2) }
       i += 1
+      this_step.distance_next_step = (response["trips"][0]["legs"][i]['distance']/1_000).round(2)
+      this_step.save
       # define the order of the trip steps
       step_to_order = @trip.steps.where(id: this_step.id).first
       step_to_order.order = i
